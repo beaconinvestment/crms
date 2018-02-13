03fe628bad62fb165502e86e1b98350dddbf4f08
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2018-02-13 16:04:31 +0500

    i have added the migrations table and migrated them to the databse named beaconscoth tables are created since there was no clearification on columns right now created dummy columns

--- beacon/database/migrations/2018_02_13_105812_create_investmenttype_table.php
+++ beacon/database/migrations/2018_02_13_105812_create_investmenttype_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateInvestmenttypeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,15 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('investment_type', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('type');
+
+            $table->integer('customer_id'); // which bear climbs this tree
+
+            $table->timestamps();
         });
     }
 
@@ -27,6 +32,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('investmenttype');
     }
 }
